967e264cf2562b45492f2d35729b00b45f377f93
akshay <akshayraut080@gmail.com>
2025-06-09 12:28:15 +0530

    commited by me

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+		Student student=(Student)context.getBean("s");
+		
+		System.out.println("student data >> "+student);
+	}
+
+}
